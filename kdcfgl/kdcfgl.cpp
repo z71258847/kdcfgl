9eab3b929f1c0c7c8086e32774d95d1e50ada02b
Yungang Wang <wangyg@umich.edu>
2020-12-09 04:56:11 +0000

    topological sort from a start block until its immediate post dominator

--- kdcfgl/kdcfgl.cpp
+++ kdcfgl/kdcfgl.cpp
@@ -4,6 +4,8 @@
 #include "llvm/Analysis/BranchProbabilityInfo.h"
 #include "llvm/Analysis/BlockFrequencyInfo.h"
 #include "llvm/Support/Format.h"
+#include "llvm/Analysis/PostDominators.h"
+#include <unordered_set>
 
 using namespace llvm;
 
@@ -16,6 +18,7 @@ struct kdcfgl : public FunctionPass {
 	void getAnalysisUsage(AnalysisUsage &AU) const{
 		AU.addRequired<BlockFrequencyInfoWrapperPass>();
 		AU.addRequired<BranchProbabilityInfoWrapperPass>();
+		AU.addRequired<PostDominatorTreeWrapperPass>();
 	}
 	
 
@@ -26,6 +29,31 @@ struct kdcfgl : public FunctionPass {
 		//
 		
 		return true;
+	}
+
+	void getKeyDependentBranchedBlocks(BasicBlock* start_block, std::vector<BasicBlock*>& sorted_blocks) {
+		PostDominatorTree &PDT = getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
+		std::unordered_set<BasicBlock*> visited;
+		getKeyDependentBranchedBlocksByTopologicalSort(start_block, start_block, PDT, visited, sorted_blocks);
+	}
+
+	void getKeyDependentBranchedBlocksByTopologicalSort(const BasicBlock* start_block, BasicBlock* block, const PostDominatorTree &PDT,
+		std::unordered_set<BasicBlock*>& visited, std::vector<BasicBlock*>& sorted_blocks) {
+		visited.insert(block);
+		if (start_block != block) {
+			bool immediate_post_dominator = PDT.dominates(block, start_block);
+			if (immediate_post_dominator) {
+				sorted_blocks.push_back(block);
+				return;
+			}
+		}
+		for (BasicBlock* suc : successors(block)) {
+			auto it = visited.find(suc);
+			if (it == visited.end()) {
+				getKeyDependentBranchedBlocksByTopologicalSort(start_block, suc, PDT, visited, sorted_blocks);
+			}
+		}
+		sorted_blocks.push_back(block);
 	}
 }; // end of struct Hello
 }  // end of anonymous namespace
