acad18496debd7ee32e7b30749437bb6d6f17f3c
Yiqin Qiu <qiuyiqin@eecs583a.eecs.umich.edu>
2020-12-09 07:22:22 +0000

    test scripts

--- script/test4.c
+++ script/test4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	int i=0, ans0=0, ans1=0, ans2=0;
+	int i=0, ans0=0, ans1=0, ans2=0, ans3=0;
 	__attribute__((annotate(""))) int key;
 	key=7;
 	for (i=0; i<8; i++){
@@ -15,8 +15,13 @@ int main(){
 			}
 		}
 		else{
-			ans2++;
+			if (i%2==1){
+				ans2++;
+			}
+			else{
+				ans3++;
+			}
 		}
 	}
-	printf("%d %d %d\n", ans0, ans1, ans2);
+	printf("%d %d %d %d\n", ans0, ans1, ans2, ans3);
 }
