ffe33162f2cc0009027136afe3801b0b80f1074b
Yungang Wang <wangyg@umich.edu>
2020-12-10 21:23:51 +0000

    get rid of the len parameter instead, get size from sizeof(T) add rdtsc to read timestamp

--- script/mod_exp.cpp
+++ script/mod_exp.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
 #include <cinttypes>
+#include <x86intrin.h>
 
-template <class T> T mod_exp(size_t len, T base, __attribute__((annotate(""))) T exp, T mod) {
+template<typename T> T mod_exp(T base, __attribute__((annotate(""))) T exp, const T mod) {
+    const size_t len = sizeof(T) << 3;
     T result = 1;
-    base = base % mod;
+    base %= mod;
     for (size_t i = 0; i < len; i++) {
         if (exp & 1) {
             result = (result * base) % mod;
@@ -15,11 +17,19 @@ template <class T> T mod_exp(size_t len, T base, __attribute__((annotate(""))) T
 }
 
 int main() {
-    uint32_t base = 0x9E6BF27A;
-    uint32_t mod = 524287;
-    uint32_t exp = 0xDEADBEEF;
-    size_t len = 32;
-    uint32_t result = mod_exp<uint32_t>(len, base, exp, mod);
-    printf("%u\n", result);
+    uint32_t base = 0xDEADBEEF;
+    uint32_t mod = (1 << 16) - 123;
+    uint32_t exp = 0x0;
+    uint64_t tstart = __rdtsc();
+    uint32_t result = mod_exp<uint32_t>(base, exp, mod);
+    uint64_t tend = __rdtsc();
+    fprintf(stderr, "all zeros, time: %lu\n", tend - tstart);
+    fprintf(stdout, "%u\n", result);
+    exp = 0xFFFFFFFF;
+    tstart = __rdtsc();
+    result = mod_exp<uint32_t>(base, exp, mod);
+    tend = __rdtsc();
+    fprintf(stderr, "all ones, time: %lu\n", tend - tstart);
+    fprintf(stdout, "%u\n", result);
     return 0;
 }
