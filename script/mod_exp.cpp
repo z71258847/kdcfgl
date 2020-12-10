e5996988db70a902f4f6a817f5fd528d97488832
Yungang Wang <wangyg@umich.edu>
2020-12-10 03:02:34 +0000

    move annotation to underlying function

--- script/mod_exp.cpp
+++ script/mod_exp.cpp
@@ -1,15 +1,15 @@
 #include <cstdio>
 #include <cinttypes>
 
-template <class T> T mod_exp(T base, T exp, T mod) {
+template <class T> T mod_exp(size_t len, T base, __attribute__((annotate(""))) T exp, T mod) {
     T result = 1;
     base = base % mod;
-    while (exp > 0) {
+    for (size_t i = 0; i < len; i++) {
         if (exp & 1) {
             result = (result * base) % mod;
         }
-        exp = exp >> 1;
-        base = (base * base) % mod;
+        exp >>= 1;
+        base = (base * base) % mod;;
     }
     return result;
 }
@@ -17,8 +17,9 @@ template <class T> T mod_exp(T base, T exp, T mod) {
 int main() {
     uint32_t base = 0x9E6BF27A;
     uint32_t mod = 524287;
-    __attribute__((annotate(""))) uint32_t exp = 0xDEADBEEF;
-    uint32_t result = mod_exp<uint32_t>(base, exp, mod);
+    uint32_t exp = 0xDEADBEEF;
+    size_t len = 32;
+    uint32_t result = mod_exp<uint32_t>(len, base, exp, mod);
     printf("%u\n", result);
     return 0;
 }
