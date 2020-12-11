e7c4a7195628cb46b81b274601d494d8ad2d6c42
Yungang Wang <wangyg@umich.edu>
2020-12-11 22:21:13 +0000

    fix mask generation and assign proper name to masks

--- kdcfgl/kdcfgl.cpp
+++ kdcfgl/kdcfgl.cpp
@@ -88,6 +88,7 @@ struct kdcfgl : public FunctionPass {
 		getKeyDependentBranchedBlocks(critical_branch, sorted_blocks);
 		print_related_blocks_in_topological_order(sorted_blocks);
 		createMasksForKeyDependentBranchedBlocks(critical_branch);
+		print_masks();
 		return true;
 	}
 
@@ -95,6 +96,14 @@ struct kdcfgl : public FunctionPass {
 		errs() << "print related blocks in topological order:\n";
 		for (auto bb : sorted_blocks) {
 			errs() <<  bb->getName() << "\n";
+		}
+	}
+
+	void print_masks() const {
+		errs() << "print masks:\n";
+		for (auto pair : masks) {
+			errs() << "(" << (pair.first.first != nullptr ? pair.first.first->getName() : "null") << ", " << pair.first.second->getName() << "): "
+			 << (pair.second != nullptr ? pair.second->getName() : "null") << ", " << (uintptr_t)pair.second << "\n";
 		}
 	}
 
@@ -135,16 +144,16 @@ struct kdcfgl : public FunctionPass {
 	void createMasksForKeyDependentBranchedBlocks(std::unordered_set<BasicBlock*> start_blocks) {
 		PostDominatorTree &PDT = getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
 		for (auto it_start_block = start_blocks.begin(); it_start_block != start_blocks.end(); it_start_block++) {
-			std::pair<BasicBlock*, BasicBlock*> edge = std::make_pair(*it_start_block, *it_start_block);
+			std::pair<BasicBlock*, BasicBlock*> edge = std::make_pair(nullptr, *it_start_block);
 			auto it_visited = masks.find(edge);
 			if (it_visited == masks.end()) {
-				masks.insert(std::make_pair(edge, nullptr));
-				createMasksForKeyDependentBranchedBlocksDFS(start_blocks, *it_start_block, *start_blocks.begin(), PDT);
+				//masks.insert(std::make_pair(edge, nullptr));
+				createMasksForKeyDependentBranchedBlocksDFS(start_blocks, *it_start_block, nullptr, *start_blocks.begin(), PDT);
 			}
 		}
 	}
 
-	void createMasksForKeyDependentBranchedBlocksDFS(const std::unordered_set<BasicBlock*> start_blocks, const BasicBlock* start_block, BasicBlock* block, const PostDominatorTree &PDT) {
+	void createMasksForKeyDependentBranchedBlocksDFS(const std::unordered_set<BasicBlock*> start_blocks, const BasicBlock* start_block, BasicBlock* from, BasicBlock* block, const PostDominatorTree &PDT) {
 		if (start_block != block) {
 			bool immediate_post_dominator = PDT.dominates(block, start_block);
 			if (immediate_post_dominator) {
@@ -154,94 +163,68 @@ struct kdcfgl : public FunctionPass {
 				}
 			}
 		}
+		std::pair<BasicBlock*, BasicBlock*> start_edge = std::make_pair(nullptr, block);
+		auto it_start_edge = masks.find(start_edge);
+		std::pair<BasicBlock*, BasicBlock*> entry_edge = std::make_pair(from, block);
+		if (from == nullptr) {	// directly called by createMasksForKeyDependentBranchedBlocks
+			masks.insert(std::make_pair(start_edge, nullptr));
+		} else {	// recursively called by createMasksForKeyDependentBranchedBlocksDFS
+			auto it_entry_edge = masks.find(entry_edge);
+			if (it_start_edge == masks.end() || it_start_edge->second == nullptr) {	// no exising start mask
+				masks[start_edge] = it_entry_edge->second;
+			} else {	// update exising start mask
+				Instruction* exisiting_start_mask = dyn_cast<Instruction>(it_start_edge->second);
+				Twine mask_name = Twine("mask_").concat(block->getName());
+				if (exisiting_start_mask->getParent() != block) {	// exising start mask is NOT in the block
+					Instruction* first_instruction = &(*block->begin());
+					it_start_edge->second = BinaryOperator::Create(BinaryOperator::Or, exisiting_start_mask, it_entry_edge->second, mask_name, first_instruction);
+				} else {	// exising start mask is in the block
+					it_start_edge->second = BinaryOperator::Create(BinaryOperator::Or, exisiting_start_mask, it_entry_edge->second, mask_name, exisiting_start_mask->getNextNode());
+				}
+			}
+		}
+		it_start_edge = masks.find(start_edge);
 		for (BasicBlock::iterator I = block->begin(), E = block->end(); I != E; ++I) {
 			unsigned int opCode = I->getOpcode();
 			if (opCode == Instruction::Br) {
 				BranchInst* brI = dyn_cast<BranchInst>(I);
 				if (brI->isConditional()) {
-					Value* predicate = brI->getOperand(0);
+					Value* predicate = brI->getCondition();
 					BasicBlock* suc = brI->getSuccessor(0);
 					std::pair<BasicBlock*, BasicBlock*> edge = std::make_pair(block, suc);
 					auto it_edge = masks.find(edge);
 					if (it_edge == masks.end()) {
-						std::pair<BasicBlock*, BasicBlock*> entry = std::make_pair(block, block);
-						auto it_entry = masks.find(entry);
-						std::pair<BasicBlock*, BasicBlock*> suc_entry = std::make_pair(suc, suc);
-						auto it_suc_entry = masks.find(suc_entry);
-						if (it_entry == masks.end() || it_entry->second == nullptr) {
+						if (it_start_edge == masks.end() || it_start_edge->second == nullptr) {
 							masks.insert(make_pair(edge, predicate));
-							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
-								masks[suc_entry] = predicate;
-							} else {
-								BinaryOperator* suc_entry_edge;
-								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, predicate, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
-								} else {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, predicate, "", &(suc->front()));
-								}
-								masks[suc_entry] = suc_entry_edge;
-							}
 						} else {
-							BinaryOperator* mask_edge = BinaryOperator::Create(BinaryOperator::And, it_entry->second, predicate, "", brI);
+							Twine mask_name = Twine("mask_").concat(block->getName()).concat("_").concat(suc->getName());
+							BinaryOperator* mask_edge = BinaryOperator::Create(BinaryOperator::And, it_start_edge->second, predicate, mask_name, brI);
+							//errs() << mask_edge->getName() << ", " << (uintptr_t)mask_edge << "\n";
 							masks.insert(make_pair(edge, mask_edge));
-							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
-								masks[suc_entry] = mask_edge;
-							} else {
-								BinaryOperator* suc_entry_edge;
-								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
-								} else {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", &(suc->front()));
-								}
-								masks[suc_entry] = suc_entry_edge;
-							}
 						}
-						createMasksForKeyDependentBranchedBlocksDFS(start_blocks, start_block, suc, PDT);
+						createMasksForKeyDependentBranchedBlocksDFS(start_blocks, start_block, block, suc, PDT);
 					}
 					suc = brI->getSuccessor(1);
 					edge = std::make_pair(block, suc);
 					it_edge = masks.find(edge);
 					if (it_edge == masks.end()) {
-						BinaryOperator* not_predicate = BinaryOperator::CreateNot(predicate, "", brI);
-						std::pair<BasicBlock*, BasicBlock*> entry = std::make_pair(block, block);
-						auto it_entry = masks.find(entry);
-						std::pair<BasicBlock*, BasicBlock*> suc_entry = std::make_pair(suc, suc);
-						auto it_suc_entry = masks.find(suc_entry);
-						if (it_entry == masks.end() || it_entry->second == nullptr) {
+						Twine mask_not_name = Twine("mask_not_").concat(predicate->getName());
+						BinaryOperator* not_predicate = BinaryOperator::CreateNot(predicate, mask_not_name, brI);
+						if (it_start_edge == masks.end() || it_start_edge->second == nullptr) {
 							masks.insert(make_pair(edge, not_predicate));
-							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
-								masks[suc_entry] = predicate;
-							} else {
-								BinaryOperator* suc_entry_edge;
-								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, not_predicate, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
-								} else {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, not_predicate, "", &(suc->front()));
-								}
-								masks[suc_entry] = suc_entry_edge;
-							}
 						} else {
-							BinaryOperator* mask_edge = BinaryOperator::Create(BinaryOperator::And, it_entry->second, not_predicate, "", brI);
+							Twine mask_name = Twine("mask_").concat(block->getName()).concat("_").concat(suc->getName());
+							BinaryOperator* mask_edge = BinaryOperator::Create(BinaryOperator::And, it_start_edge->second, not_predicate, mask_name, brI);
+							//errs() << mask_edge->getName() << ", " << (uintptr_t)mask_edge << "\n";
 							masks.insert(make_pair(edge, mask_edge));
-							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
-								masks[suc_entry] = mask_edge;
-							} else {
-								BinaryOperator* suc_entry_edge;
-								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
-								} else {
-									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", &(suc->front()));
-								}
-								masks[suc_entry] = suc_entry_edge;
-							}
 						}
-						createMasksForKeyDependentBranchedBlocksDFS(start_blocks, start_block, suc, PDT);
+						createMasksForKeyDependentBranchedBlocksDFS(start_blocks, start_block, block, suc, PDT);
 					}
 				}
 			}
 		}
 	}
-}; // end of struct Hello
+}; // end of struct kdcfgl
 }  // end of anonymous namespace
 
 char kdcfgl::ID = 0;
