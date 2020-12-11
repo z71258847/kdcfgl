2d7b5541a538256fa0defca9c6fa739a1d3e05b7
Yungang Wang <wangyg@umich.edu>
2020-12-11 09:30:40 +0000

    mask generation

--- kdcfgl/kdcfgl.cpp
+++ kdcfgl/kdcfgl.cpp
@@ -6,9 +6,12 @@
 #include "llvm/Analysis/BlockFrequencyInfo.h"
 #include "llvm/Support/Format.h"
 #include "llvm/Analysis/PostDominators.h"
+#include "llvm/IR/InstrTypes.h"
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 #include <deque>
+#include <utility>
 
 
 using namespace llvm;
@@ -18,8 +21,18 @@ bool isBranch(unsigned opCode){
 		opCode==Instruction::IndirectBr;
 }
 
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const std::pair<T1, T2>& p) const { 
+        auto hash1 = std::hash<T1>()(p.first); 
+        auto hash2 = std::hash<T2>()(p.second); 
+        return (hash1*17) ^ hash2; 
+    } 
+};
+
 namespace {
 struct kdcfgl : public FunctionPass {
+	std::unordered_map<std::pair<BasicBlock*, BasicBlock*>, Value*, hash_pair> masks;
 	static char ID;
 	kdcfgl() : FunctionPass(ID) {}
 
@@ -74,6 +87,7 @@ struct kdcfgl : public FunctionPass {
 		std::vector<BasicBlock*> sorted_blocks;
 		getKeyDependentBranchedBlocks(critical_branch, sorted_blocks);
 		print_related_blocks_in_topological_order(sorted_blocks);
+		createMasksForKeyDependentBranchedBlocks(critical_branch);
 		return true;
 	}
 
@@ -116,6 +130,116 @@ struct kdcfgl : public FunctionPass {
 			}
 		}
 		sorted_blocks.push_back(block);
+	}
+
+	void createMasksForKeyDependentBranchedBlocks(std::unordered_set<BasicBlock*> start_blocks) {
+		PostDominatorTree &PDT = getAnalysis<PostDominatorTreeWrapperPass>().getPostDomTree();
+		for (auto it_start_block = start_blocks.begin(); it_start_block != start_blocks.end(); it_start_block++) {
+			std::pair<BasicBlock*, BasicBlock*> edge = std::make_pair(*it_start_block, *it_start_block);
+			auto it_visited = masks.find(edge);
+			if (it_visited == masks.end()) {
+				masks.insert(std::make_pair(edge, nullptr));
+				createMasksForKeyDependentBranchedBlocksDFS(start_blocks, *it_start_block, *start_blocks.begin(), PDT);
+			}
+		}
+	}
+
+	void createMasksForKeyDependentBranchedBlocksDFS(const std::unordered_set<BasicBlock*> start_blocks, const BasicBlock* start_block, BasicBlock* block, const PostDominatorTree &PDT) {
+		if (start_block != block) {
+			bool immediate_post_dominator = PDT.dominates(block, start_block);
+			if (immediate_post_dominator) {
+				auto it = start_blocks.find(block);
+				if (it == start_blocks.end()) {
+					return;
+				}
+			}
+		}
+		for (BasicBlock::iterator I = block->begin(), E = block->end(); I != E; ++I) {
+			unsigned int opCode = I->getOpcode();
+			if (opCode == Instruction::Br) {
+				BranchInst* brI = dyn_cast<BranchInst>(I);
+				if (brI->isConditional()) {
+					Value* predicate = brI->getOperand(0);
+					BasicBlock* suc = brI->getSuccessor(0);
+					std::pair<BasicBlock*, BasicBlock*> edge = std::make_pair(block, suc);
+					auto it_edge = masks.find(edge);
+					if (it_edge == masks.end()) {
+						std::pair<BasicBlock*, BasicBlock*> entry = std::make_pair(block, block);
+						auto it_entry = masks.find(entry);
+						std::pair<BasicBlock*, BasicBlock*> suc_entry = std::make_pair(suc, suc);
+						auto it_suc_entry = masks.find(suc_entry);
+						if (it_entry == masks.end() || it_entry->second == nullptr) {
+							masks.insert(make_pair(edge, predicate));
+							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
+								masks[suc_entry] = predicate;
+							} else {
+								BinaryOperator* suc_entry_edge;
+								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, predicate, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
+								} else {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, predicate, "", &(suc->front()));
+								}
+								masks[suc_entry] = suc_entry_edge;
+							}
+						} else {
+							BinaryOperator* mask_edge = BinaryOperator::Create(BinaryOperator::And, it_entry->second, predicate, "", brI);
+							masks.insert(make_pair(edge, mask_edge));
+							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
+								masks[suc_entry] = mask_edge;
+							} else {
+								BinaryOperator* suc_entry_edge;
+								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
+								} else {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", &(suc->front()));
+								}
+								masks[suc_entry] = suc_entry_edge;
+							}
+						}
+						createMasksForKeyDependentBranchedBlocksDFS(start_blocks, start_block, suc, PDT);
+					}
+					suc = brI->getSuccessor(1);
+					edge = std::make_pair(block, suc);
+					it_edge = masks.find(edge);
+					if (it_edge == masks.end()) {
+						BinaryOperator* not_predicate = BinaryOperator::CreateNot(predicate, "", brI);
+						std::pair<BasicBlock*, BasicBlock*> entry = std::make_pair(block, block);
+						auto it_entry = masks.find(entry);
+						std::pair<BasicBlock*, BasicBlock*> suc_entry = std::make_pair(suc, suc);
+						auto it_suc_entry = masks.find(suc_entry);
+						if (it_entry == masks.end() || it_entry->second == nullptr) {
+							masks.insert(make_pair(edge, not_predicate));
+							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
+								masks[suc_entry] = predicate;
+							} else {
+								BinaryOperator* suc_entry_edge;
+								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, not_predicate, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
+								} else {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, not_predicate, "", &(suc->front()));
+								}
+								masks[suc_entry] = suc_entry_edge;
+							}
+						} else {
+							BinaryOperator* mask_edge = BinaryOperator::Create(BinaryOperator::And, it_entry->second, not_predicate, "", brI);
+							masks.insert(make_pair(edge, mask_edge));
+							if (it_suc_entry == masks.end() || it_suc_entry->second == nullptr) {
+								masks[suc_entry] = mask_edge;
+							} else {
+								BinaryOperator* suc_entry_edge;
+								if (dyn_cast<Instruction>(it_suc_entry->second)->getParent() == suc) {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", dyn_cast<Instruction>(it_suc_entry->second)->getNextNode());
+								} else {
+									suc_entry_edge = BinaryOperator::Create(BinaryOperator::Or, it_suc_entry->second, mask_edge, "", &(suc->front()));
+								}
+								masks[suc_entry] = suc_entry_edge;
+							}
+						}
+						createMasksForKeyDependentBranchedBlocksDFS(start_blocks, start_block, suc, PDT);
+					}
+				}
+			}
+		}
 	}
 }; // end of struct Hello
 }  // end of anonymous namespace
